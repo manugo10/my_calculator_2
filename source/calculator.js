e799e9862be6790d8ec3cf115023f2fe3e69228c
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2019-02-04 20:32:36 -0600

    sin(x) button

--- source/calculator.js
+++ source/calculator.js
@@ -1,5 +1,6 @@
 let pow3 = document.querySelector("#pow3");
 let pow4 = document.querySelector("#pow4");
+let sin = document.querySelector("#sinx");
 pow3.addEventListener("click", function () {
     let value = document.querySelector("#number1");
     value.value = pow(value.value, 3);
@@ -8,6 +9,12 @@ pow4.addEventListener("click", function () {
     let value = document.querySelector("#number1");
     value.value = pow(value.value, 4);
 });
+
+sin.addEventListener("click", function () {
+    let value = document.querySelector("#number1");
+    value.value = Math.sin(value.value);
+});
+
 // @ return pow  
 function pow(value, pow) {
     return Math.pow(value, pow);
